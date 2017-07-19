390b0dd11bb3acf51bed4382d91bef15daa64c2e
Maria Schreiber <meschreiber3@gmail.com>
2017-07-19 12:38:04 -0400

    Set-up basic file structure

---
+++ browser/js/app.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+//import { Provider } from 'react-redux'
+//import store from './store'
+//import Routes from './Routes'
+  // <Provider store={store}>
+  // </Provider>,
+
+ReactDOM.render(
+  <div>Good morning, world.</div>,
+  document.getElementById('app')
+)
